f6d6c31d9dfb32d7822526004889cdbd6dd1a74f
Justin Wheeler <justinwwheeler@gmail.com>
2021-08-20 19:51:23 +0000

    adds function call to get pass times and log to console

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
-//   console.log("It worked. Returned IP:", ip);
-// });
+/** Next ISS Times:
+ * Use this function call to initiate a series of API calls
+ * Result is a log out of the next 5 times when the ISS will pass over the users location based on thier IP address
+ */
 
-// fetchCoordsByIP('75.156.173.19', (error, data) => {
-//   console.log(error);
-//   console.log(data);
-// });
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) return console.log('Function failed: ', error);
 
-// fetchISSFlyOverTimes({ latitude: 51.0857, longitude: -114.0234 }, (error, data) => {
-//   console.log(error);
-//   console.log(data);
-// });
+  for (const time of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(time.risetime);
+    console.log(`Next pass at ${datetime} for ${time.duration} seconds!`);
+  }
+});
